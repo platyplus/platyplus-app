bdbe3e759c2e31de2b0a805c3891995dcbb67db5
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-01-14 14:04:47 +0100

    minor adjustment in the template

--- src/pages/Stage.vue
+++ src/pages/Stage.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
-  q-page(padding class="justify-center")
-    div(v-if="details")
-      router-link(:to="'/workflow/'+workflow_id") Back to {{item.workflow.name}}
-      q-field(label="Name")
-        q-input(:readonly="reading" v-model="form.name" ref="firstInput" @keyup.enter="save")
+  q-page(v-if="details" padding class="justify-center")
+    router-link(:to="'/workflow/'+workflow_id") Back to {{item.workflow.name}}
+    q-field(label="Name")
+      q-input(:readonly="reading" v-model="form.name" ref="firstInput" @keyup.enter="save")
     button-bar(:reading="reading" :details="details" @create="create" @edit="edit" @save="save" @reset="reset" @cancel="cancel" @remove="remove")
 </template>
 
