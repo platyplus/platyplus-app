459a645acd4ae4da03d614f0cbb6552ea20c0dd8
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-01-27 22:52:54 +0100

    diff

--- scripts/deploy-documentation.sh
+++ scripts/deploy-documentation.sh
@@ -7,14 +7,13 @@ set -e
 npm run docs:build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
-
-git init
-git add -A
 if [[ ! `git diff-index --quiet HEAD docs/.vuepress/dist` ]]; then
-  echo changes
-  git commit -m 'deploy'
-  git push -f https://plmercereau:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git master
+    echo changes
+    cd docs/.vuepress/dist
+    git init
+    git add -A
+    git commit -m 'deploy'
+    git push -f https://plmercereau:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git master
 else
   echo nochanges
 fi
