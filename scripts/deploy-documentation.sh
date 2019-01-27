3991195b1566c0874301a5ce239c1c5acb122155
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-01-27 23:35:21 +0100

    bug

--- scripts/deploy-documentation.sh
+++ scripts/deploy-documentation.sh
@@ -24,7 +24,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 git add -A
 
-if [[ ! `git diff-index --quiet HEAD docs/.vuepress/dist` ]]; then
+if [[ ! `git diff-index --quiet HEAD` ]]; then
     # deploy to github pages
     git commit -m 'deploy'
     git push https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git master
