aee0b76aa5b64494ef2cabf9506e03572e7bbffe
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-01-27 23:25:43 +0100

    better script

--- scripts/deploy-documentation.sh
+++ scripts/deploy-documentation.sh
@@ -9,14 +9,27 @@ export npm_config_loglevel=error
 # Install depedencies
 npm install
 
+# TODO: run tests
+# TODO: enable a smart way to deploy:
+# 1. pull/clone the github gh page repo into the ci image
+# 2. build the dist
+# 3. if dist files changed, commit and push
+cd docs/.vuepress/dist
+git clone https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git
+cd -
+
 # build the docs
 npm run docs:build
 
-# deploy to github pages
 cd docs/.vuepress/dist
-git init
 git add -A
-git commit -m 'deploy'
-git push -f https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git master
+
+if [[ ! `git diff-index --quiet HEAD docs/.vuepress/dist` ]]; then
+    # deploy to github pages
+    git commit -m 'deploy'
+    git push https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git master
+else
+    echo "no changes"
+fi
 
 cd -
--- scripts/set-github-ci.sh
+++ scripts/set-github-ci.sh
@@ -6,4 +6,3 @@ set -e
 git config --global user.email "no-reply@platy.plus"
 git config --global user.name "Drone.io CI"
 
-cd -
