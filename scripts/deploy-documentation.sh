67c354292758a3e6470941c877edb390479ad885
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-01-27 23:15:26 +0100

    final one?

--- scripts/deploy-documentation.sh
+++ scripts/deploy-documentation.sh
@@ -2,18 +2,21 @@
 
 # abort on errors
 set -e
-git diff-index HEAD https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git/docs
-# navigate into the build output directory
-if [[ ! `git diff-index --quiet HEAD https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git/docs` ]]; then
-    export npm_config_loglevel=error
-    npm install
-    # build the docs
-    npm run docs:build
-    cd docs/.vuepress/dist
-    git init
-    git add -A
-    git commit -m 'deploy'
-    git push -f https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git master
-fi
+
+# change npm verbosity
+export npm_config_loglevel=error
+
+# Install depedencies
+npm install
+
+# build the docs
+npm run docs:build
+
+# deploy to github pages
+cd docs/.vuepress/dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f https://$GH_USER:$GH_TOKEN@github.com/platyplus/platyplus.github.io.git master
 
 cd -
