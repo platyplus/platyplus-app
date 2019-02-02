f5e54aec4a13b6f595bec0449846e7543b8550af
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-02-02 12:34:45 +0100

    log

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ const app = express()
 
 app.use(history())
 app.use(serveStatic(path.join(__dirname, '/dist/spa-mat')))
+app.use(function (req, res, next) {
+  res.setHeader('Set-Cookie', process.env.APP_CONF)
+  next()
+})
 app.listen(port)
+console.log(`Server running on port ${port}`)
