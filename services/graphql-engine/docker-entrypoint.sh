d507368049e0ca87b4b5a201a17b5cf83a390ce9
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-02-11 00:58:01 +0630

    debug.....

--- services/graphql-engine/docker-entrypoint.sh
+++ services/graphql-engine/docker-entrypoint.sh
@@ -29,7 +29,9 @@ file_env() {
 }
 
 file_env 'HASURA_GRAPHQL_ACCESS_KEY'
+echo "${HASURA_GRAPHQL_ACCESS_KEY}"
 file_env 'AUTH_PUBLIC_KEY'
+echo "${AUTH_PUBLIC_KEY}"
 export "HASURA_GRAPHQL_JWT_SECRET"="{\"type\":\"${AUTH_ALGORITHM}\", \"key\":\"${AUTH_PUBLIC_KEY}\"}"
 # TODO: debug
 echo "${HASURA_GRAPHQL_JWT_SECRET}"
