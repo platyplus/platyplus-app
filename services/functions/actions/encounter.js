e01c61653265e608b861251615e4ba891ec57cb8
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-04-02 16:06:55 +0200

    basic update of state data works!!!

--- services/functions/actions/encounter.js
+++ services/functions/actions/encounter.js
@@ -1,59 +1,162 @@
-const { ALL } = require('./helpers'),
-  graphqlQueries = require('../graphqlQueries'),
-  sqlQueries = require('../sqlQueries'),
-  { graphql, rest } = require('../clients')
+const gql = require('graphql-tag'),
+  { print } = require('graphql/language/printer'),
+  { graphql, rest } = require('../clients'),
+  { getOldNew, generateRules, ALL } = require('./helpers')
 
-const AGGREGATORS = ['last'] // The default aggregation is the first value of the array
+const ENCOUTER_DATE_FIELD = 'created_at' // TODO: not the ideal field
+
+const ENTITY_TYPE_SCHEMA_FROM_ENTITY = print(gql`
+  query entityTypeSchema($id: uuid!) {
+    entity(where: { id: { _eq: $id } }) {
+      type {
+        schema
+      }
+    }
+  }
+`)
+
+const STATES_FROM_ENTITY = print(gql`
+  query entityStates($id: uuid!, $encounter_date: timestamptz!) {
+    entity(where: { id: { _eq: $id } }) {
+      states(
+        where: {
+          _and: [
+            { date_start: { _lte: $encounter_date } }
+            { derived: { date_end: { _gt: $encounter_date } } }
+          ]
+        }
+      ) {
+        id
+        stage {
+          id
+          schema
+        }
+        date_start
+        derived {
+          date_end
+        }
+      }
+    }
+  }
+`)
+
+const UPDATE_ENTITY_ATTRIBUTES = print(gql`
+  mutation updateEntityAttributes($id: uuid!, $attributes: jsonb!) {
+    update_entity(
+      where: { id: { _eq: $id } }
+      _set: { attributes: $attributes }
+    ) {
+      affected_rows
+    }
+  }
+`)
+
+const UPDATE_STATE_DATA = print(gql`
+  mutation updateStateData($id: uuid!, $data: jsonb!) {
+    update_state(where: { id: { _eq: $id } }, _set: { data: $data }) {
+      affected_rows
+    }
+  }
+`)
+
+const SQL_AGGREGATE_ENTITY_DATA = (id, rules) =>
+  `select jsonb_build_object( ${rules} ) as data
+  from (
+    select entity_id, data
+    from encounter
+    where entity_id = '${id}'
+    order by ${ENCOUTER_DATE_FIELD} asc) as ordered_encounter  
+  group by entity_id
+  `
+
+const SQL_AGGREGATE_STATE_DATA = (entityId, dateStart, dateEnd, rules) =>
+  `select jsonb_build_object( ${rules} ) as data
+  from (
+    select entity_id, data
+    from encounter
+    where entity_id = '${entityId}' and ${ENCOUTER_DATE_FIELD} >= '${dateStart}' and ${ENCOUTER_DATE_FIELD} < '${dateEnd}'
+    order by created_at asc ) as ordered_encounter
+  group by entity_id`
 
 /**
  * Updates the entity attributes from its encounters data
- * TODO: this function works but HAS TO BE TESTED EXTENSIVELY
  * @param {*} ctx
  */
-// TODO: to consider: then for encounter: on create: check if one entity exists. if not, create one. It will then trigger an update
-// TODO: then on update and on delete, update the entity attributes (still check if entity_id exists)
 const updateEntityAttributes = async ctx => {
   const {
     event: { data }
   } = ctx.request.body
-  // Picks the entity ids from the old and the new encounters (in case the entity changed in an update)
-  let entityIds = []
-  for (let x in data) {
-    data[x].entity_id &&
-      !entityIds.includes(data[x].entity_id) &&
-      entityIds.push(data[x].entity_id)
-  }
-  for (const id of entityIds) {
+  for (const oldNew of getOldNew(ctx, ['entity_id'])) {
+    const id = data[oldNew].entity_id
     // Gets the schema from the entity type
-    const { entity } = await graphql.request(
-      graphqlQueries.ENTITY_TYPE_SCHEMA_FROM_ENTITY,
-      { id }
-    )
+    const { entity } = await graphql.request(ENTITY_TYPE_SCHEMA_FROM_ENTITY, {
+      id
+    })
     const schema = entity[0].type.schema // TODO: abort if (although abnormal) there is no type or no schema
-    // Generates the aggregation rules from the schema
-    const rules = []
-    for (let x in schema) {
-      // do not include if by any chance the field x equals false
-      if (schema[x]) {
-        const aggregator = AGGREGATORS.includes(schema[x])
-          ? schema[x]
-          : AGGREGATORS[0]
-        rules.push(`'${x}', ${aggregator}((data->>'${x}')::text)`)
-      }
-    }
+    const rules = generateRules(schema)
     // Calculates the entity data from the rules
     const {
       data: { result }
     } = await rest.post('', {
       type: 'run_sql',
-      args: { sql: sqlQueries.SQL_AGGREGATE_ENTITY_DATA(id, rules) }
+      args: { sql: SQL_AGGREGATE_ENTITY_DATA(id, rules) }
     })
-    const attributes = JSON.parse(result[1][0])
     // Updates the entity attributes
-    await graphql.request(graphqlQueries.UPDATE_ENTITY_ATTRIBUTES, {
+    await graphql.request(UPDATE_ENTITY_ATTRIBUTES, {
       id,
-      attributes
+      attributes: JSON.parse(result[1][0])
     })
   }
 }
-module.exports = ALL(updateEntityAttributes)
+
+/**
+ * Updates the data of every state that includes the encounter (old and new date, old and new entity)
+ * @param {*} ctx
+ */
+const updateStageData = async ctx => {
+  const {
+    event: { data }
+  } = ctx.request.body
+  for (const oldNew of getOldNew(ctx, ['entity_id', ENCOUTER_DATE_FIELD])) {
+    const entityId = data[oldNew].entity_id
+    // Select all the states impacted by this encounter change
+    const { entity } = await graphql.request(STATES_FROM_ENTITY, {
+      id: entityId,
+      encounter_date: data[oldNew][ENCOUTER_DATE_FIELD]
+    })
+    const states = entity[0].states
+    // TODO: reuse this loop for a trigger on state. BE THEN CAREFULL TO AN INFINITE LOOP!!!
+    for (const state of states) {
+      const {
+        id,
+        stage: { schema }, // eslint-disable-next-line camelcase
+        date_start, // eslint-disable-next-line camelcase
+        derived: { date_end }
+      } = state
+      // TODO: abort if (although abnormal) there is no type or no schema
+      const rules = generateRules(schema)
+      // Selects and aggregates all the encounters of this entity that occurred during the state period
+      const {
+        data: { result }
+      } = await rest.post('', {
+        type: 'run_sql',
+        args: {
+          sql: SQL_AGGREGATE_STATE_DATA(entityId, date_start, date_end, rules)
+        }
+      })
+      // Updates the state data accordingly
+      await graphql.request(UPDATE_STATE_DATA, {
+        id,
+        data: JSON.parse(result[1][0])
+      })
+    }
+  }
+}
+
+// TODO: Avoid infinite loop => STRICT RULE NOT TO UPDATE THE RECORD OF THE EVENT (or find a workaround)
+// TODO: to consider: then for encounter: on create: check if one entity exists. if not, create one. It will then trigger an update
+// TODO: then on update and on delete, update the entity attributes (still check if entity_id exists)
+module.exports = ALL(async ctx => {
+  await updateEntityAttributes(ctx)
+  await updateStageData(ctx)
+})
--- services/functions/actions/index.js
+++ services/functions/actions/index.js
@@ -5,7 +5,6 @@ const undefinedTableAction = async ctx => {
   console.log('TODO: undefined table action')
 }
 
-// TODO: Avoid infinite loop => STRICT RULE NOT TO UPDATE THE RECORD OF THE EVENT (or find a workaround)
 module.exports = {
   encounter,
   defaultAction: ALL(undefinedTableAction)
