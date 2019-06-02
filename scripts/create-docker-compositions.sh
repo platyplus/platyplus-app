6e08fd71473627de0a7ae46e8c6f0d09b6c3f64c
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-06-02 20:22:38 +0200

    hasura beta 2

--- scripts/create-docker-compositions.sh
+++ scripts/create-docker-compositions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Automatically invoqued at every git commit
 # Install the script with: npm i -g merge-yaml-cli
 merge-yaml -i docker-compose.yml docker-compose-prod.yml -o portainer-stack.yml
 merge-yaml -i docker-compose.yml docker-compose-prod.yml docker-compose-ssl.yml -o portainer-stack-ssl.yml
