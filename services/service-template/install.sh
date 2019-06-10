227d15ace95c00500be0442f8dfd596b9eda8044
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-06-10 09:11:29 +0200

    auto-reload package.json in the microservices

--- services/service-template/install.sh
+++ services/service-template/install.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 mv /opt/app/package*.json* /opt/
 cd /opt
 npm config set '@bit:registry' https://node.bit.dev
-npm install --no-optional
-npm install @godaddy/terminus
+npm install --no-optional . @godaddy/terminus
 npm cache clean --force
+chown -R node .
