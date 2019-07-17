93eb997fa59b0c1466a22ce3608b47126d5246d6
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-07-17 19:36:37 +0200

    cleanup

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -3,7 +3,6 @@
     q-input(icon="fas fa-user" label="Login" helper="Enter your user name" error-label="We need a valid user name" v-model="username" ref="username" @keyup.enter="login")
     q-input(icon="fas fa-key" label="Password" helper="Enter your password" error-label="We need a valid password" type="password" v-model="password" @keyup.enter="login")
     q-btn(icon="fas fa-sign-in-alt" label="Sign in" @click="login")
-    json-editor(v-model="json" :schema="schema")
 </template>
 
 <style>
@@ -15,43 +14,7 @@ export default {
   name: 'PageSignIn',
   data: () => ({
     username: '',
-    password: '',
-    json: {},
-    schema: {
-      type: 'object',
-      title: 'A custom form',
-      properties: {
-        name: {
-          title: 'Full Name',
-          $ref: '#/definitions/name'
-        },
-        calc: {
-          title: 'Calculation',
-          $ref: '#/definitions/calc'
-        },
-        color: {
-          title: 'Color',
-          $ref: '#/definitions/color'
-        }
-      },
-      definitions: {
-        name: {
-          type: 'string',
-          minLength: 5
-        },
-        calc: {
-          type: 'string',
-          template: '{{cname}}!!!',
-          watch: {
-            cname: 'name'
-          }
-        },
-        color: {
-          type: 'string',
-          format: 'color'
-        }
-      }
-    }
+    password: ''
   }),
   methods: {
     async login (e) {
