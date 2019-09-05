c6d3df57afc5f0a3df96aa42b70b7345d147435e
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-09-05 14:25:20 +0200

    give up the sharing of templates

--- src/components/hasura-table/fields/edit/ManyToMany.vue
+++ src/components/hasura-table/fields/edit/ManyToMany.vue
@@ -1,22 +1,34 @@
 <template lang="pug">
-Select(v-if="element" :multiple="true"
-  :property="property"
-  :element="element"
-  :tableClass="property.class"
-  v-model="formValue")
+div(v-if="element")
+  slot(name="before-field" :property="property" :element="element")
+  slot(name="field" :property="property"  :element="element")
+    q-select(:label="$t(tableName +'.labels.'+name)"
+      filled
+      multiple
+      use-chips
+      :hint="$t(tableName +'.helpers.'+name)"
+      hide-hint
+      :error-label="$t(tableName +'.errors.'+name)"
+      v-model="formValue"
+      :options="options"
+      option-value="_id"
+      option-label="_label"
+      :clearable="!property.required"
+      use-input
+      input-debounce="0"
+      @filter="filterOptions"
+      :key="name"
+      :name="name"
+      stack-label)
+  slot(name="after-field" :property="property" :element="element")
 div(v-else) Element does not exist
 </template>
 
 <script lang="ts">
 import { Component, Mixins } from 'vue-property-decorator'
 import { FieldManyToManyMixin, FieldOptionsMixin } from 'src/boot/hasura'
-import Select from './Select.vue'
 
-@Component({
-  components: {
-    Select
-  }
-})
+@Component
 export default class EditManyToManyField extends Mixins(
   FieldOptionsMixin,
   FieldManyToManyMixin
