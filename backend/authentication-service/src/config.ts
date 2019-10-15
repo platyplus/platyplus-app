b791918f7e0dd74c85509f8cfc049d992ab06699
Pierre-Louis Mercereau <24897252+plmercereau@users.noreply.github.com>
2019-10-15 13:59:39 +0200

    feat: define the JWT expiration
    affects: @platyplus/authentication-service

--- backend/authentication-service/src/config.ts
+++ backend/authentication-service/src/config.ts
@@ -4,3 +4,4 @@ export const PUBLIC_KEY: string = (process.env.PUBLIC_KEY || '').replace(
 )
 export const PRIVATE_KEY = (process.env.PRIVATE_KEY || '').replace(/\\n/g, '\n')
 export const ALGORITHM = process.env.ALGORITHM || 'RS256'
+export const EXPIRES_IN = process.env.EXPIRES_IN || '15m'
